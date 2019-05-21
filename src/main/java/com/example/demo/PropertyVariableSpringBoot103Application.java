8ce138a6d6603832412014ce7e1f7ef963c55b90
ghorellana <ghorellana@gmail.com>
2019-05-21 11:37:43 -0400

    Testing 103

---
+++ src/main/java/com/example/demo/PropertyVariableSpringBoot103Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyVariableSpringBoot103Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyVariableSpringBoot103Application.class, args);
+	}
+
+}
